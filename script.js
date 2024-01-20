7a6e991e1c103c836e05ec07d650bd2c86711b37
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-01-20 14:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
-    //Write your code here
+const form = document.getElementById("form1");
 
+function getFormvalue() {
+  let x = document.forms["form1"]["fname"].value;
+  let y = document.forms["form1"]["lname"].value;
+  alert(x+" "+y);  
 }
+
+
